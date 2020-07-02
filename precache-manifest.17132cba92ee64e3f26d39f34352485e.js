70e2dec813e045afc821cd57f4d6b9b81e6984f1
Tamara Vahinerii Flosse <tflosse@Tamaras-MacBook-Pro.local>
2020-07-01 22:49:23 -0700

    Updates

---
+++ precache-manifest.17132cba92ee64e3f26d39f34352485e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831d52b2ea3b5280587d59d3fc4a6ad7",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "dec23c5d50276e68cc9c",
+    "url": "/React-Project/static/css/main.59b8b0f6.chunk.css"
+  },
+  {
+    "revision": "b95124ce5d1cd2fdd57b",
+    "url": "/React-Project/static/js/2.34c100b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Project/static/js/2.34c100b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec23c5d50276e68cc9c",
+    "url": "/React-Project/static/js/main.7ecfaf5b.chunk.js"
+  },
+  {
+    "revision": "9b387df1b6279f1600e9",
+    "url": "/React-Project/static/js/runtime-main.b24ec088.js"
+  }
+]);
