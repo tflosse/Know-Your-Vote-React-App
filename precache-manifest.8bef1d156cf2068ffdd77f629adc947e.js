79e5b71238b3fe284ec2383e0167a8f96f0cde24
Tamara Vahinerii Flosse <tflosse@Tamaras-MacBook-Pro.local>
2020-07-02 11:44:52 -0700

    Updates

---
+++ precache-manifest.8bef1d156cf2068ffdd77f629adc947e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c5ba14ec293179cdda0fc61be9f985",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "b13e6850bed28bc968d9",
+    "url": "/React-Project/static/css/main.c2ec90ab.chunk.css"
+  },
+  {
+    "revision": "7e0c2cd84e6bc5300e49",
+    "url": "/React-Project/static/js/2.b2f046cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Project/static/js/2.b2f046cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13e6850bed28bc968d9",
+    "url": "/React-Project/static/js/main.937df19f.chunk.js"
+  },
+  {
+    "revision": "9b387df1b6279f1600e9",
+    "url": "/React-Project/static/js/runtime-main.b24ec088.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Project/precache-manifest.17132cba92ee64e3f26d39f34352485e.js"
+  "/React-Project/precache-manifest.8bef1d156cf2068ffdd77f629adc947e.js"
 );
 
 self.addEventListener('message', (event) => {
